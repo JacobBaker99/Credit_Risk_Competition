8336fc58b5797502d380751eca91179e89165cb9
Jacob <116323299+JacobBaker99@users.noreply.github.com>
2024-04-10 16:10:32 -0400

    Added the file of all of out python models.

---
+++ All Python Models/cs3270p1_the_front_line_multinomialnb_alpha_1.2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+'''
+multinomial naive bayes for Project One
+
+alpha = 1.2
+
+This module is used to conduct multinomial naive bayes on dev.csv
+'''
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+
+
+# pylint: disable=C0103
+
+__author__ = 'Frontline'
+__version__ = 'Spring 2024'
+__pylint__ = '2.14.5'
+
+def prepData():
+    '''
+    Prepare the data from the csv for k-fold and multinomial naive bayes
+    '''
+    data = pd.read_csv(r'dev.csv')
+    data.drop("case_id", axis=1, inplace=True)
+    data = data.astype(str)
+
+    for column in data.columns:
+        if data[column].dtype == 'object':  # Checking for string/object type
+            data[column].fillna("0", inplace=True)
+
+
+    feature_list = [
+        "birth_259D","contaddr_smempladdr_334L",
+        "empl_employedtotal_800L","empl_industry_691L",
+        "familystate_447L","housetype_905L","incometype_1044T",
+        "mainoccupationinc_384A","num_group1","personindex_1023L",
+        "persontype_1072L","persontype_792L","role_1084L",
+        "safeguarantyflag_411L","sex_738L","type_25L"
+    ]
+
+    features = data[feature_list]
+
+    labels = data['target']
+
+    categorical_features = [
+        "birth_259D","contaddr_smempladdr_334L","empl_employedtotal_800L",
+        "empl_industry_691L","familystate_447L","housetype_905L",
+        "incometype_1044T","mainoccupationinc_384A","num_group1",
+        "personindex_1023L","persontype_1072L","persontype_792L",
+        "role_1084L","safeguarantyflag_411L","sex_738L","type_25L"
+    ]
+
+
+    one_hot = OneHotEncoder()
+    transformer = ColumnTransformer([("one_hot", one_hot,
+                    categorical_features)], remainder="passthrough")
+    encoded_features = transformer.fit_transform(features).toarray()
+    one_hot_features = transformer.named_transformers_['one_hot']
+    one_hot_features = one_hot_features.get_feature_names_out(categorical_features)
+    remaining_features = [f for f in features.columns if f not in categorical_features]
+    all_feature_names = list(one_hot_features) + remaining_features
+    assert len(all_feature_names) == encoded_features.shape[1], "Mismatch in feature count"
+    return [encoded_features, all_feature_names, labels]
+
+def analyze(encoded_features, all_feature_names, labels):
+    '''
+    Execute k-fold and multinomial naive bayes
+    '''
+    hyper = [3270, 5, 1.2]
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(pd.DataFrame(encoded_features,
+                        columns=all_feature_names))
+
+    skf = StratifiedKFold(n_splits=hyper[1],
+                            shuffle=True, random_state=hyper[0])
+    for train_index, test_index in skf.split(X_scaled, labels):
+        X_train, X_test = X_scaled[train_index], X_scaled[test_index]
+        y_train, y_test = labels.iloc[train_index], labels.iloc[test_index]
+
+        mnb_model = MultinomialNB(alpha=hyper[2])
+        mnb_model.fit(X_train, y_train)
+        print(f'\tMNB: {accuracy_score(y_test, mnb_model.predict(X_test)) * 100:.2f}%')
+
+
+    mnb_scores = cross_val_score(MultinomialNB(alpha=hyper[2]),
+                    X_scaled, labels, cv=skf, scoring="accuracy")
+    print(f'Multinomial Naive Bayes Scores: {mnb_scores.mean():.4f} ± {mnb_scores.std():.4f}')
+
+if __name__ == "__main__":
+    dataArray = prepData()
+    analyze(dataArray[0], dataArray[1], dataArray[2])
---
+++ All Python Models/cs3270p1_the_front_line_random_forest_25_trees.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+'''
+Random Forest for Project One
+
+This module is used to conduct decision trees on train_person1.csv, train_person2.csv,
+and train_base.csv. This is the default Deision tree from sklearn with 25 trees.
+'''
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn import ensemble
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+
+# pylint: disable=C0103
+
+__author__ = 'Frontline'
+__version__ = 'Spring 2024'
+__pylint__ = '2.14.5'
+
+SEED = 3270
+
+def fit():
+    """
+    Fit the data to the model.
+    """
+     # We need to turn data strings into numbers that Sklearn can process, onehotencoding
+    one_hot = OneHotEncoder()
+    trans = ColumnTransformer([("one_hot", one_hot, cat_features)], remainder="passthrough")
+    encoded_features = trans.fit_transform(features).toarray()
+    one_hot_features = trans.named_transformers_['one_hot'].get_feature_names_out(cat_features)
+    remaining_features = [f for f in features.columns if f not in cat_features]
+    all_feature_names = list(one_hot_features) + remaining_features
+    assert len(all_feature_names) == encoded_features.shape[1], "Mismatch in feature count"
+
+    X = pd.DataFrame(encoded_features, columns=all_feature_names)
+    y = labels
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)  # X is your dat
+    return X_scaled, y
+
+def test(X_scaled, y):
+    """
+    Testing the folds of the fitted data. And a test of the overall accuracy.
+    """
+    NUM_FOLDS = 5
+    TREES = 25
+
+    skf = StratifiedKFold(n_splits=NUM_FOLDS, shuffle=True, random_state=SEED)
+    for train_index, test_index in skf.split(X_scaled, y):
+        X_train, X_test = X_scaled[train_index], X_scaled[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        rf_model = ensemble.RandomForestClassifier(criterion='entropy', n_estimators=TREES,
+            random_state=SEED)
+        rf_model.fit(X_train, y_train)
+        print(f'\tRF_entropy: {accuracy_score(y_test, rf_model.predict(X_test)) * 100:.2f}%')
+
+    rf_scores = cross_val_score(ensemble.RandomForestClassifier(criterion='entropy',
+        n_estimators=TREES, random_state=SEED), X_scaled, y, cv=skf, scoring="accuracy")
+    print(f'Random Forest Scores: {rf_scores.mean():.2f} ± {rf_scores.std():.2f}')
+
+if __name__ == '__main__':
+    data = pd.read_csv(r'dev.csv')
+    data.drop("case_id", axis=1, inplace=True)
+    data = data.astype(str)
+
+    for column in data.columns:
+        if data[column].dtype == 'object':  # Checking for string/object type
+            data[column].fillna("0", inplace=True)
+
+    feature_list = [
+    "birth_259D","contaddr_smempladdr_334L","empl_employedtotal_800L",
+        "empl_industry_691L","familystate_447L","housetype_905L",
+        "incometype_1044T","mainoccupationinc_384A","num_group1",
+        "personindex_1023L","persontype_1072L","persontype_792L",
+        "role_1084L","safeguarantyflag_411L","sex_738L","type_25L"
+    ]
+    features = data[feature_list]
+    labels = data['target']
+
+    cat_features = [
+    "birth_259D","contaddr_smempladdr_334L","empl_employedtotal_800L",
+    "empl_industry_691L","familystate_447L","housetype_905L",
+    "incometype_1044T","mainoccupationinc_384A","num_group1",
+    "personindex_1023L","persontype_1072L","persontype_792L",
+    "role_1084L","safeguarantyflag_411L","sex_738L","type_25L"
+    ]
+
+    X_scale, Y = fit()
+    test(X_scale, Y)
